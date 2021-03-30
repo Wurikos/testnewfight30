bd192a87d1030170669cb7399b257d60bd490dc0
Sulin Alexander <50768680+Wurikos@users.noreply.github.com>
2021-03-30 11:17:45 +0300

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6062dcf5f11f97001e8142bc",
+				"6062dcf5f11f97001e8142be"
+			]
+		},
+		"6062dcf5f11f97001e8142bc": {
+			"id": "6062dcf5f11f97001e8142bc",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6062dcf5f11f97001e8142be": {
+			"id": "6062dcf5f11f97001e8142be",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
